cc60568a5df96ed127cd3a7e1c82d08f9fbfc1ae
Franco Valdes <franco.valdes89@gmail.com>
2019-02-14 22:49:29 -0500

    updating conditional in template

--- src/assetbundles/resources/src/js/modules/if.js
+++ src/assetbundles/resources/src/js/modules/if.js
@@ -4,17 +4,21 @@ export function onInit(context) {
     context.querySelectorAll('[data-if]').forEach((node) => {
         const selector = node.getAttribute('data-if');
         const operator = node.getAttribute('data-if-value');
+        const or = node.getAttribute('data-or-value') || operator;
         const trigger = context.querySelector(`#${selector}`);
 
         const initialValue = trigger.value || "0";
 
         if (initialValue != operator) {
             node.classList.add('hidden');
+            if (or && (initialValue != or)) {
+                node.classList.add('hidden');
+            }
         }
 
         trigger.addEventListener('change', (e) => {
             const newValue = e.target.value;
-            if (newValue == operator) {
+            if (newValue == operator || newValue == or) {
                 node.classList.remove('hidden');
             } else {
                 node.classList.add('hidden');
